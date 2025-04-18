56e8fa9180c6bd87b1c7330c0df789c4bd33a509
Alex <alejandromontes393@gmail.com>
2025-04-18 13:52:37 -0600

    feat: add Colors constant for light and dark theme management

---
+++ constants/Colors.ts
@@ -0,0 +1,22 @@
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
+
+export const Colors = {
+  light: {
+    text: "#11181C",
+    background: "#7752CC",
+    secondaryBackground: "#E6E2D2",
+    tint: tintColorLight,
+    icon: "#7752CC",
+    tabIconDefault: "#687076",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#ECEDEE",
+    background: "#151718",
+    tint: tintColorDark,
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
+    tabIconSelected: tintColorDark,
+  },
+};
